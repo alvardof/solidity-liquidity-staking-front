ce579bf18693739e153a6c21afb8522b49198b33
Gorka <gorkaemaldi@gmail.com>
2022-04-20 00:31:12 -0400

    staking rewards update

---
+++ src/context/StakingRewards/index.jsx
@@ -0,0 +1,19 @@
+import { createContext, useEffect, useContext, useState } from "react";
+import stakingRewardsInfo from "../../contracts/StakingRewards.json";
+import { ethers } from "ethers";
+import WalletContext from "../Wallet";
+
+const StakeRewardsContext = createContext();
+
+export function DropFactoryProvider({ children }) {
+    const address = stakingRewardsInfo.address;
+    const abi = stakingRewardsInfo.abi;
+    const { signer } = useContext(WalletContext);
+    const [stakingReward, setStakingReward] = useState(null);
+
+    return (
+        <StakeManagerContext.Provider value={{ address, abi, stakingManager }}>
+          {children}
+        </StakeManagerContext.Provider>
+      );
+}
